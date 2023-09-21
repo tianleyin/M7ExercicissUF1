99e8b8e0de80520dd2dcd1a5b0e7bde78daa0c26
Tianle <tianleyin8888@gmail.com>
2023-09-21 19:52:32 +0200

    Cambios guerraNaval_0

--- guerraNaval.php
+++ guerraNaval.php
@@ -1,47 +1,95 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial scale=1">
     <title></title>
     <style type="text/css">
-        table, td {
-            border: 1px black solid;
+        table {
             border-collapse: collapse;
+        }
+        td {
+            border: 1px black solid;
+            width: 30px;
+            height: 30px;
         }
     </style>
 </head>
+
 <body>
     <h1>Guerra Naval</h1>
     <table>
-    <?php
-    $n = 7;
-    $str = 'a';
-    $subm = [rand(1,4)=>rand(1,7)];
-    foreach($subm as $y => $x){
-        echo $y;
-        echo $x;
-    }
-    echo "  <tr>";
-    echo "\t\t<td> </td>\n";
-    for ($i=1;$i<=$n;$i++){
-        echo "\t\t<td> $i </td>\n";
-    }
-    echo "  </tr>";
-    for ($i2=1;$i2<=4;$i2++){
-        echo "  <tr>";
-        for ($i=1;$i<=4;$i++){
-            echo "\t\t<td> $str </td>\n";
-            for ($i=1;$i<=$n;$i++){
-                if($y==1 && $x==1){
-                    echo "\t\t<td>X</td>\n";
-                }
-            }
-            ++$str;
+        <?php
+
+        $mida = 10;
+
+        // Portaavions
+        $_SESSION['portaavions'] = [];
+        $ship_x4 = rand(0, $mida - 4);
+        $ship_y4 = rand(0, $mida -1);
+        $_SESSION['portaavions'][] = ['x' => $ship_x4, 'y' => $ship_y4];
+
+        // Destructor
+        $_SESSION['destructor'] = [];
+        for ($i = 0; $i < 2; $i++) {
+            $ship_x3 = rand(0, $mida - 3);
+            $ship_y3 = rand(0, $mida - 1);
+            $_SESSION['destructor'][] = ['x' => $ship_x3, 'y' => $ship_y3];
         }
-        echo "  </tr>";
-    }
-    ?>
+
+        // Submari
+        $_SESSION['submari'] = [];
+        for ($i = 0; $i < 3; $i++) {
+            $ship_x2 = rand(0, $mida - 2);
+            $ship_y2 = rand(0, $mida - 1);
+            $_SESSION['submari'][] = ['x' => $ship_x2, 'y' => $ship_y2];
+        }
+
+        // Fragata
+        $_SESSION['fragata'] = [];
+        for ($i = 0; $i < 4; $i++) {
+            $ship_x1 = rand(0, $mida - 2);
+            $ship_y1 = rand(0, $mida - 1);
+            $_SESSION['fragata'][] = ['x' => $ship_x1, 'y' => $ship_y1];
+        }
+
+        for ($fila = 0; $fila < $mida; $fila++) {
+            echo "<tr>";
+            for ($col = 0; $col < $mida; $col++) {
+                $contingut = "&nbsp;";
+                foreach ($_SESSION['fragata'] as $sub) {
+                    if ($col == $sub['x'] && $fila == $sub['y']) {
+                        $contingut = "F";
+                        break;
+                    }
+                }
+                foreach ($_SESSION['destructor'] as $dest) {
+                    if (
+                        $col >= $dest['x'] &&
+                        $col < $dest['x'] + 3 &&
+                        $fila == $dest['y']
+                    ) {
+                        $contingut = "D";
+                        break;
+                    }
+                }
+                foreach ($_SESSION['submari'] as $sub) {
+                    if (
+                        $col >= $sub['x'] &&
+                        $col < $sub['x'] + 2 &&
+                        $fila == $sub['y']
+                    ) {
+                        $contingut = "S";
+                        break;
+                    }
+                }
+                echo "<td>$contingut</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
     </table>
 </body>
+
 </html>
